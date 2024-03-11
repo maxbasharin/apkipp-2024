f9e274b8de336b7715c7437a411792c4b0ed80e2
Max Basharin <maxbasharinweb@gmail.com>
2024-03-11 18:11:00 +0300

    11.03.24

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.dropdown').hover(
-        function() {
-         $(this).find('.dropdown-menu').stop().fadeIn(100);
+        function () {
+            $(this).find('.dropdown-menu').stop().fadeIn(100);
         },
-        function() {
-         $(this).find('.dropdown-menu').stop().fadeOut(100);
+        function () {
+            $(this).find('.dropdown-menu').stop().fadeOut(100);
         }
     );
+ 
+    $('.header__menu-opener').click(function() {
+        $('.main-menu').fadeToggle(200);
+        $('.overlay').toggleClass('active');
+        
     });
+
+});
