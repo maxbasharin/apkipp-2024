125d1c0ba86a8d048c2d196aebe91d029bfb3811
Max Basharin <maxbasharinweb@gmail.com>
2024-03-15 12:03:27 +0300

    завершил первую версию

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     /* Бургер навигация */
     $('.header__menu-opener').click(function () {
         $('.main-menu').fadeToggle(200);
-        $('.navigation').css('position', 'static');
+        $('.navigation__section').fadeToggle(200);
         $("body").toggleClass("overflow-hidden");
 
         if ($("body").hasClass("overflow-hidden")) {
@@ -20,22 +20,17 @@ $(document).ready(function () {
             $('body').css('overflow', 'auto');
         }
     });
-
-
-
     /* Модальное окно popup */
     $('.popup-btn').click(function () {
         $('body').css('overflow', 'hidden');
         $('.popup').fadeIn();
         return false;
     });
-
     $('.popup-close').click(function () {
         $('body').css('overflow', 'auto');
         $(this).parents('.popup').fadeOut();
         return false;
     });
-
     $(document).keydown(function (e) {
         if (e.keyCode === 27) {
             e.stopPropagation();
@@ -43,19 +38,14 @@ $(document).ready(function () {
             $('.popup').fadeOut();
         }
     });
-
     $('.popup').click(function (e) {
         if ($(e.target).closest('.popup-content').length == 0) {
             $('body').css('overflow', 'auto');
             $(this).fadeOut();
         }
     });
-
     // клик по иконке WhatsApp
     $('body').on('click', '.wthsbutton', function (e) {
         window.open('https://api.whatsapp.com/send?phone=79613084929', '_blank');
     });
-
-
-
 });
