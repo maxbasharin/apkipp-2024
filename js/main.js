7c611dbe489edc65b2b2ca2afe839db4fc8d25fa
Max Basharin <maxbasharinweb@gmail.com>
2024-05-15 17:29:32 +0300

    Страница Кафедры завершена

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+
+  // Отложения загрузка
+  const observer = lozad();
+  observer.observe();
+  lozad('.lozad', {
+    load: function (el) {
+      el.src = el.dataset.src;
+      el.onload = function () {
+        el.classList.add('fade')
+      }
+    }
+  }).observe()
+
+
   // Выпадающие списки шапки
   $(".dropdown").hover(
     function () {
@@ -51,232 +65,202 @@ $(document).ready(function () {
   });
 
 
-let isMobile = {
-  Android: function () {
-    return navigator.userAgent.match(/Android/i);
-  },
-  BlackBerry: function () {
-    return navigator.userAgent.match(/BlackBerry/i);
-  },
-  iOS: function () {
-    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-  },
-  Opera: function () {
-    return navigator.userAgent.match(/Opera Mini/i);
-  },
-  Windows: function () {
-    return navigator.userAgent.match(/IEMobile/i);
-  },
-  any: function () {
-    return (
-      isMobile.Android() ||
-      isMobile.BlackBerry() ||
-      isMobile.iOS() ||
-      isMobile.Opera() ||
-      isMobile.Windows()
-    );
-  },
-};
+  let isMobile = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+      return (
+        isMobile.Android() ||
+        isMobile.BlackBerry() ||
+        isMobile.iOS() ||
+        isMobile.Opera() ||
+        isMobile.Windows()
+      );
+    },
+  };
 
-// Фотогалерея кафедры
-const gallery = document.querySelector(".gallery__body");
-let galleryItems, galleryColumn, speed, positionX, coordXpercentage;
+  // Фотогалерея кафедры
+  const gallery = document.querySelector(".gallery__body");
+  let galleryItems, galleryColumn, speed, positionX, coordXpercentage;
 
-if (gallery && !isMobile.any()) {
-  galleryItems = document.querySelector(".gallery__items");
-  galleryColumn = document.querySelectorAll(".gallery__column");
+  if (gallery && !isMobile.any()) {
+    galleryItems = document.querySelector(".gallery__items");
+    galleryColumn = document.querySelectorAll(".gallery__column");
 
-  // Скорость анимации
-  speed = gallery.dataset.speed;
+    // Скорость анимации
+    speed = gallery.dataset.speed;
 
-  positionX = 0;
-  coordXpercentage = 0;
+    positionX = 0;
+    coordXpercentage = 0;
 
-  gallery.addEventListener("mousemove", function (e) {
-    const furnitureWidth = gallery.offsetWidth;
+    gallery.addEventListener("mousemove", function (e) {
+      const furnitureWidth = gallery.offsetWidth;
 
-    const coordX = e.pageX - furnitureWidth / 2;
+      const coordX = e.pageX - furnitureWidth / 2;
 
-    coordXpercentage = (coordX / furnitureWidth) * 200;
+      coordXpercentage = (coordX / furnitureWidth) * 200;
 
-    if (!gallery.classList.contains("_init")) {
-      requestAnimationFrame(setMouseGalleryStyle);
-      gallery.classList.add("_init");
-    }
-  });
-}
-
-// Прослеживание движения мыши
-function setMouseGalleryStyle() {
-  let galleryItemsWidth = 0;
-  galleryColumn.forEach((element) => {
-    galleryItemsWidth += element.offsetWidth;
-  });
-
-  const furnitureDifferent = galleryItemsWidth - gallery.offsetWidth;
-  const distX = Math.floor(coordXpercentage - positionX);
-
-  positionX = positionX + distX * speed;
-  let position = (furnitureDifferent / 200) * positionX;
-
-  galleryItems.style.cssText = `transform: translate3d(${-position}px,0,0);`;
-
-  if (Math.abs(distX) > 0) {
-    requestAnimationFrame(setMouseGalleryStyle);
-  } else {
-    gallery.classList.remove("_init");
+      if (!gallery.classList.contains("_init")) {
+        requestAnimationFrame(setMouseGalleryStyle);
+        gallery.classList.add("_init");
+      }
+    });
   }
-}
 
-//  // Отложения загрузка
-//  const observer = lozad();
-//  observer.observe();
+  // Прослеживание движения мыши
+  function setMouseGalleryStyle() {
+    let galleryItemsWidth = 0;
+    galleryColumn.forEach((element) => {
+      galleryItemsWidth += element.offsetWidth;
+    });
 
-//  lozad('.lozad', {
-//          load: function(el) {
-//                  el.src = el.dataset.src;
-//                  el.onload = function() {
-//                          el.classList.add('fade')
-//                  }
-//          }
-//  }).observe()
+    const furnitureDifferent = galleryItemsWidth - gallery.offsetWidth;
+    const distX = Math.floor(coordXpercentage - positionX);
 
-// Стандартный слайдер контента
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    720: {
-      slidesPerView: 4,
-    },
-    480: {
-      slidesPerView: 2,
-    },
-    230: {
-      slidesPerView: 1,
-    },
-  },
-});
+    positionX = positionX + distX * speed;
+    let position = (furnitureDifferent / 200) * positionX;
 
-// Слайдер отзывов с счетчиком
-var swiperReviews = new Swiper(".swiper-reviews", {
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+    galleryItems.style.cssText = `transform: translate3d(${-position}px,0,0);`;
 
-// Слайдер видео-отзывов с счетчиком
-var swiperReviewsVideo = new Swiper(".swiper-reviews-video", {
-  slidesPerView: 2,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    720: {
-      slidesPerView: 2,
-    },
-    480: {
-      slidesPerView: 1,
-    },
-    200: {
-      slidesPerView: 1,
-    },
-  },
-});
+    if (Math.abs(distX) > 0) {
+      requestAnimationFrame(setMouseGalleryStyle);
+    } else {
+      gallery.classList.remove("_init");
+    }
+  }
 
-// Навигация скролл по странице
-$(".refers a").on("click", function (event) {
+  // Стандартный слайдер контента
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      720: {
+        slidesPerView: 4,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+      230: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  // Слайдер отзывов с счетчиком
+  var swiperReviews = new Swiper(".swiper-reviews", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+  // Навигация скролл по странице
+  $(".refers a").on("click", function (event) {
     event.preventDefault();
     var target = $(this).attr("href");
     var targetPosition = $(target).offset().top - 110;
 
     $("html, body").animate(
-     {
+      {
         scrollTop: targetPosition,
-     },
-     300
+      },
+      300
     );
-});
+  });
 
-// Выделение активного пункта при скролле навигации
-$(window).on("scroll", function () {
-  var scrollPosition = $(window).scrollTop() + 120;
-  
-  $(".sidebar__nav a").each(function () {
+  // Выделение активного пункта при скролле навигации
+  $(window).on("scroll", function () {
+    var scrollPosition = $(window).scrollTop() + 120;
+
+    $(".sidebar__nav a").each(function () {
       var target = $(this).attr("href");
       var targetElement = $(target);
-      
+
       if (targetElement.length) {
-       var targetPosition = targetElement.offset().top;
-       var targetHeight = targetElement.outerHeight();
-  
-       if (
+        var targetPosition = targetElement.offset().top;
+        var targetHeight = targetElement.outerHeight();
+
+        if (
           scrollPosition >= targetPosition &&
           scrollPosition < targetPosition + targetHeight
-       ) {
+        ) {
           $(".sidebar__nav a").removeClass("active");
           $(this).addClass("active");
-       } else {
+        } else {
           $(this).removeClass("active");
-       }
+        }
       }
-  });
+    });
   });
 
-// Раскрывающий список
-$(".content__accordion .content__accordion-item .content__accordion-header"),
-  $(".content__accordion-header").on("click", function () {
-    $(".icon-plus").removeClass("active");
-    $(this).hasClass("active")
-      ? ($(this).siblings(".content__accordion-text").slideUp(),
-        $(this).removeClass("active"),
-        $(this).find(".icon-plus").removeClass("active"))
-      : ($(".content__accordion-text").slideUp(),
-        $(".content__accordion-header").removeClass("active"),
-        $(this).siblings(".content__accordion-text").slideToggle(),
-        $(this).toggleClass("active"),
-        $(this).find(".icon-plus").addClass("active"));
-  });
+  // Раскрывающий список вопрос ответ
+  $(".content__accordion .content__accordion-item .content__accordion-header"),
+    $(".content__accordion-header").on("click", function () {
+      $(".icon-plus").removeClass("active");
+      $(this).hasClass("active")
+        ? ($(this).siblings(".content__accordion-text").slideUp(),
+          $(this).removeClass("active"),
+          $(this).find(".icon-plus").removeClass("active"))
+        : ($(".content__accordion-text").slideUp(),
+          $(".content__accordion-header").removeClass("active"),
+          $(this).siblings(".content__accordion-text").slideToggle(),
+          $(this).toggleClass("active"),
+          $(this).find(".icon-plus").addClass("active"));
+    });
 
   // Скрывать контент когда ширина меньше 997px 
-  $(window).resize(function() {
+  $(window).resize(function () {
     var screenWidth = $(window).width();
     var contentItemWrapper = $('.content__item-wrapper');
 
     if (screenWidth <= 997) {
-     contentItemWrapper.addClass('js-accordion-content');
+      contentItemWrapper.addClass('js-accordion-content');
     } else {
-     contentItemWrapper.removeClass('js-accordion-content');
+      contentItemWrapper.removeClass('js-accordion-content');
     }
-});
-
-$(".js-accordion .js-accordion-item .js-accordion-toggle"),
-  $(".js-accordion-toggle").on("click", function () {
-    $(".js-accordion-icon").removeClass("active");
-    $(this).hasClass("active")
-      ? ($(this).siblings(".js-accordion-content").slideUp(),
-        $(this).removeClass("active"),
-        $(this).find(".js-accordion-icon").removeClass("active"))
-      : ($(".js-accordion-content").slideUp(),
-        $(".js-accordion-toggle").removeClass("active"),
-        $(this).siblings(".js-accordion-content").slideToggle(),
-        $(this).toggleClass("active"),
-        $(this).find(".js-accordion-icon").addClass("active"));
   });
 
+  // Раскрывающий список стандартный
+  $(".js-accordion-toggle").on("click", function () {
+    $(this).siblings(".js-accordion-content").slideToggle();
+    $(this).toggleClass("active");
+    $(this).find(".js-accordion-icon").toggleClass("active");
+    });
+});
+
+  // Плашка подать заявку
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var documentHeight = $(document).height();
+    var applyHeight = $('.apply').outerHeight();
+
+    if (scroll > 0) {
+        $('.apply').addClass('active');
+    } else {
+        $('.apply').removeClass('active');
+    }
 });
